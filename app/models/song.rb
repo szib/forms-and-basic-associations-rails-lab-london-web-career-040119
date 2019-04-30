943613fd726642f5af44e7a2ff2fe0a0e265c9c1
Ivan Szebenszki <szib77@gmail.com>
2019-04-30 10:21:11 +0100

    Pass model tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,31 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :genre
+  belongs_to :artist
+  has_many :notes
+
+  def genre_name
+    genre ? genre.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    notes.map(&:content)
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      notes << Note.create(content: content) unless content.empty?
+    end
+  end
 end
